27747bf4db620f5e6924d7ac4c9eac16b8c250de
kelvinmburu <mburuhkelvin@gmail.com>
2022-08-21 15:32:11 +0300

    build: generated navabr and landing page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
